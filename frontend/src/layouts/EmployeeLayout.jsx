e71e1bcaa579f5de90734428bcd06f76597739fb
PotatoLover169 <daneric.besario@gmail.com>
2026-08-15 18:08:11 +0800

    feat: establish responsive employee layout

--- frontend/src/layouts/EmployeeLayout.jsx
+++ frontend/src/layouts/EmployeeLayout.jsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router-dom";
-
 import Sidebar from "../components/navigation/Sidebar";
+
+import "../styles/sidebar.css";
+import "../styles/layout.css";
 
 function EmployeeLayout() {
   return (
-    <div>
+    <div className="employee-layout">
       <Sidebar />
 
-      <main>
+      <main className="employee-main">
         <Outlet />
       </main>
     </div>
